89f9f6242aaf492dd398eb8f1a80b7101272d065
Ryan McCahon <mccahonr@yahoo.com>
2020-05-13 02:37:09 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,9 +2,15 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
 
+  @@account = []
+
   def initialize(name, balance = 1000, status = "open")
     @name = name
     @balance = balance
     @status = status
+  end
+
+  def deposit(money)
+    @@account << money
   end
 end
