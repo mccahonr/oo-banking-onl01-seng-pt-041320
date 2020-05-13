36e92a4f6cb072bc4dd3785f7b8242092721fe71
Ryan McCahon <mccahonr@yahoo.com>
2020-05-13 03:16:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,5 +28,12 @@ class Transfer
     end
   end
 
+  def reverse_transfer
+    if @status ==  "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = "reversed"    
+  end
+
 
 end
