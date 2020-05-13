952e5cf57c926b387f9dec11c362535b314e6195
Ryan McCahon <mccahonr@yahoo.com>
2020-05-13 03:02:53 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
       @receiver.balance += @amount
       @status = "complete"
     else
-      @staus = "rejected"
+      @status = "rejected"
       "Transaction rejected. Please check your account balance."
     end
   end
