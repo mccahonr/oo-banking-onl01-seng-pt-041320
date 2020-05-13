106df845f4a3128801a3f75fef9e8a9e171c02d0
Ryan McCahon <mccahonr@yahoo.com>
2020-05-13 02:44:50 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -15,4 +15,12 @@ class BankAccount
   def display_balance
     "Your balance is $#{self.balance}."
   end
+
+  def valid?
+    if @status == "open" && @balance > 0
+      true
+    else
+      false
+    end
+  end
 end
